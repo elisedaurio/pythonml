e508f36a2f742434cfb0e5c0b5c81eeb3c9d1c32
Eric Gruber <lonephoenix@gmail.com>
2017-05-21 22:05:52 -0400

    Initial Commit. Still installing TF and CUDA for GPU learning

---
+++ mnist.py
@@ -0,0 +1,9 @@
+# Import Tensorflow
+input tensorflow as tf
+
+
+# Load in the MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+

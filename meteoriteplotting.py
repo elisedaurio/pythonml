9016d611a90f8970a5d8eb4efa71dfe4992c4e97
Eric Gruber <lonephoenix@gmail.com>
2017-06-11 19:08:17 -0400

    Plotted the data

--- meteoriteplotting.py
+++ meteoriteplotting.py
@@ -2,14 +2,13 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Create a new csv reader
 data = pd.read_csv("cleanlandings.csv")
-print(data)
 
 # Create panda dataframe
 dataframe = pd.DataFrame(data, columns=["id", "rectype", "recmass", "recfalltype", "reclat", "reclong"])
 
-# Plot the data
-sns.jointplot(x="reclong", y="reclat", data=dataframe)
+dataframe.plot.hexbin(x="reclong", y="reclat", C="recmass", bins = "log", reduce_C_function=np.max, gridsize=50)
 plt.show()

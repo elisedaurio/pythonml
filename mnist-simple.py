dd57ed4e6828dca913403c489a7de27d7ddab90e
Eric Gruber <lonephoenix@gmail.com>
2017-06-11 16:16:34 -0400

    Minor modifications to sample files

--- mnist-simple.py
+++ mnist-simple.py
@@ -56,6 +56,7 @@ def main(_):
   # outputs of 'y', and then average across the batch.
   cross_entropy = tf.reduce_mean(
       tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+  
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
   sess = tf.InteractiveSession()
